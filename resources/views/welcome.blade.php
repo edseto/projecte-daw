a2036a5a2cdde87403afb29a0409ee593597fa84
Blue <bluestorm.android@gmail.com>
2022-11-19 11:12:03 +0100

    hc cards on welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,49 @@
 <x-app>
-    TODO: listado de habitaciones sprint 2
+    <main>
+        <div class="container-fluid bg-trasparent my-4 p-3" style="position: relative;">
+            <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+                <div class="col">
+                    <div class="card h-100 shadow-sm"> <img src="https://www.aparthotelgolfbeach.com/wp-content/uploads/2022/10/Apartament-E-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-dark" style="color:white !important;">340.00&euro;</span> <span class="float-end small text-muted">Barcelona</span> </div>
+                            <h5 class="card-title">Aparthotel Golf Beach</h5>
+                            <div class="cart-text">Type A room</div>
+                            <div class="d-grid gap-2 my-4"> <a href="#" class="btn btn-success">Reserva ara</a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100 shadow-sm"> <img src="https://www.aparthotelgolfbeach.com/wp-content/uploads/2022/10/Estudi-A-Superior-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-dark" style="color:white !important;">240.00&euro;</span> <span class="float-end small text-muted">Barcelona</span> </div>
+                            <h5 class="card-title">Aparthotel Golf Beach</h5>
+                            <div class="cart-text">Type B room</div>
+                            <div class="d-grid gap-2 my-4"> <a href="#" class="btn btn-success">Reserva ara</a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100 shadow-sm"> <img src="https://www.aparthotelgolfbeach.com/wp-content/uploads/2022/10/Estudi-B-2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-dark" style="color:white !important;">180.00&euro;</span> <span class="float-end small text-muted">Barcelona</span> </div>
+                            <h5 class="card-title">Hotel GrandSummer</h5>
+                            <div class="cart-text">Type 3 room</div>
+                            <div class="d-grid gap-2 my-4"> <a href="#" class="btn btn-success">Reserva ara</a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100 shadow-sm"> <img src="https://www.aparthotelgolfbeach.com/wp-content/uploads/2022/10/Estudi-A-Superior-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-dark" style="color:white !important;">150.00&euro;</span> <span class="float-end small text-muted">Barcelona</span> </div>
+                            <h5 class="card-title">Hotel GrandSummer</h5>
+                            <div class="cart-text">Type 4 room</div>
+                            <div class="d-grid gap-2 my-4"> <a href="#" class="btn btn-success">Reserva ara</a> </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </main>
 </x-app>
